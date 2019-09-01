7b81fb4a3089f8d090cd278e7405e92bd474e542
Rebecca Willett <willett.rebecca@gmail.com>
2019-09-01 18:48:33 +0000

    first push - goingback to react-hello-webapp

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -6,7 +6,7 @@ export class Home extends React.Component {
 	render() {
 		return (
 			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
+				<h1>Rebecca Willett</h1>
 				<p>
 					<img src={rigoImage} />
 				</p>
